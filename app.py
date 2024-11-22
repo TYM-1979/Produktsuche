52e8473fdfd45ecdbb4dc241e87ae77ee1e3c4b3
TYM-1979 <tu@cameronsino.com>
2024-11-22 13:00:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    # Render gibt den PORT über die Umgebungsvariable vor
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 
